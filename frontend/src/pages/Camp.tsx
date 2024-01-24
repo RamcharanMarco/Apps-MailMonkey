de3a7105d5a484ee9c91d6fd2fb78422c9129299
RamcharanMarco <miguelmarcoramcharan@gmail.com>
2024-01-24 23:39:13 +0200

    added more

--- frontend/src/pages/Camp.tsx
+++ frontend/src/pages/Camp.tsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Camp = () => {
   return (
-    <div>Camp</div>
+    <div>
+        <h1>campaign name</h1>
+        <div>
+            <h2>welcome email</h2>
+            <p>this is email you send to a new subscriber</p>
+            <button>view</button>
+        </div>
+        <div className="emails">
+            <p>email1</p>
+            <p>emal2</p>
+            <p>email3</p>
+            <p>email4</p>
+        </div>
+        <button>crete new mail</button>
+    </div>
   )
 }
 
--- frontend/src/pages/Console.tsx
+++ frontend/src/pages/Console.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import '../styles/console.css'
 
+const camps = [
+  {
+    id: 'uuuidqqq',
+    name: 'one'
+  },
+  {
+    id: 'heeiei',
+    name: 'two'
+  },
+  {
+    id: 'eldldld',
+    name: 'three'
+  }
+]
+
 const Console = () => {
   return (
     <div className="console">
-      
+      <div className="console-nav">
+        <button>create</button>
+      </div>
+      <div className="campaigns">
+        <p>campaign1</p>
+        <p>campaign2</p>
+      </div>
     </div>
   )
 }
